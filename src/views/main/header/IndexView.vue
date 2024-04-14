17f1e994076b88d8de08c8aaeb58a815cd0e1e63
BO <bwang0629@gmail.com>
2024-04-14 10:25:23 +0800

    添加Avatar下拉功能

--- src/views/main/header/IndexView.vue
+++ src/views/main/header/IndexView.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import { Avatar } from '@element-plus/icons-vue'
+import { Avatar, HelpFilled, SwitchButton } from '@element-plus/icons-vue'
 </script>
 <template>
   <el-row class="my-row" align="middle">
     <el-col :span="4" class="my-col">
       <h1>PlantOCR</h1>
     </el-col>
-    <el-col :span="2.5" class="my-col btn">
-      <el-icon><Avatar /></el-icon>
+    <el-col :span="2.5">
+      <el-dropdown>
+        <el-button
+          type="primary"
+          :icon="Avatar"
+          circle
+          style="outline: none; box-shadow: 5px 4px 10px #162d81db"></el-button>
+        <template #dropdown>
+          <el-dropdown-item :icon="HelpFilled">Personal Center</el-dropdown-item>
+          <el-dropdown-item :icon="SwitchButton" style="color: red; font-weight: bolder">
+            Logout
+          </el-dropdown-item>
+        </template>
+      </el-dropdown>
     </el-col>
   </el-row>
 </template>
